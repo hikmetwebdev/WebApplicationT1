f84d544a74cf9390b769b8f3f78e5589a67fed86
hikmet <hikmet_xidirzada57@mail.ru>
2024-05-14 19:38:36 +0400

    Add project files.

---
+++ WebApplicationT1/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationT1.UserControllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Hikmət";
+            ViewBag.Surname = "Xidirzadə";
+            ViewBag.Age = 25;
+            ViewBag.Country = "Azərbaycan";
+            ViewBag.Phonenumber = +994554770076;
+            return View();
+        }
+    }
+}
